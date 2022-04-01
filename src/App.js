bf2923033c391d6a4a9d7a49dab40edc0039b773
Quentin MULLIEZ <33511073+Druwww@users.noreply.github.com>
2022-03-31 21:25:12 -0400

    firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionDate } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  //config
+  apiKey: "AIzaSyCLbQ7M6BczUd0Uv8gaFkHfvzYO1X7rnvo",
+  authDomain: "partage-passions.firebaseapp.com",
+  projectId: "partage-passions",
+  storageBucket: "partage-passions.appspot.com",
+  messagingSenderId: "821577078663",
+  appId: "1:821577078663:web:be53e23df38b6cc39ea522",
+  measurementId: "G-23YBPSR142"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {console.log(firebase)}
+        hi
       </header>
     </div>
   );
